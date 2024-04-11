3e1eabf82df5245b0d1c813b5f8d177d95b22485
nicolas-limadev <nicolasbasto76@gmail.com>
2024-04-10 23:38:13 -0300

    new: add deployment nginx

---
+++ terraform/main.tf
@@ -0,0 +1,13 @@
+resource "digitalocean_database_db" "passin-db" {
+  cluster_id = digitalocean_database_cluster.postgres-example.id
+  name       = "passin-db"
+}
+
+resource "digitalocean_database_cluster" "passin-cluster" {
+  name       = "passin-cluster"
+  engine     = "pg"
+  version    = "16"
+  size       = "db-s-1vcpu-1gb"
+  region     = "nyc1"
+  node_count = 1
+}
